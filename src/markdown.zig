f941e98cbb8dc6acbb2aca326bc4ef8857a32a6b
Emmett McDow <emmett.mcdow@gmail.com>
2025-09-07 18:06:29 -0700

    Add ability to have higher degree headers

--- src/markdown.zig
+++ src/markdown.zig
@@ -51,26 +51,26 @@ pub const Lexer = struct {
         self.src = src;
 
         while (self.i < self.src.len) {
-            if (self.match(.HEADER)) {
-                self.pushToken(.HEADER);
+            if (self.match(.HEADER)) |degree| {
+                self.pushToken(.HEADER, degree);
                 self.consumeUntil('\n');
                 self.i += 1;
             } else {
                 // Is PLAIN
                 const isEmpty = self.tokens.getLastOrNull() == null;
                 if (isEmpty or self.tokens.getLast().tType != .PLAIN) {
-                    self.pushToken(.PLAIN);
+                    self.pushToken(.PLAIN, 1);
                 }
                 self.i += 1;
             }
         }
-        self.pushToken(null);
+        self.pushToken(null, null);
 
         return self.tokens.items;
     }
 
     /// Completes the token at the end of the list and optionally adds a new one of type newType.
-    fn pushToken(self: *Self, newType: ?TokenType) void {
+    fn pushToken(self: *Self, newType: ?TokenType, degree: ?u8) void {
         if (self.tokens.pop()) |token| {
             var copy = token;
             copy.endI = @min(self.i, self.src.len);
@@ -78,15 +78,25 @@ pub const Lexer = struct {
             self.tokens.append(copy) catch unreachable;
         }
         if (newType) |tt| {
-            self.tokens.append(.{ .tType = tt, .startI = self.i, .endI = self.i + 1 }) catch unreachable;
+            assert(degree != null);
+            if (degree) |d| {
+                self.tokens.append(
+                    .{ .tType = tt, .startI = self.i, .endI = self.i + 1, .degree = d },
+                ) catch unreachable;
+            }
         }
     }
 
-    /// Determines if this is the beginning of a token.
-    fn match(self: Self, t: TokenType) bool {
+    /// Determines if this is the start of a token. Returns null if not a match, degree otherwise.
+    fn match(self: Self, t: TokenType) ?u8 {
         switch (t) {
             .HEADER => {
-                return self.isNewline() and self.thisCharIs('#') and self.nextCharIs(' ');
+                if (!self.isNewline()) return null;
+                var i: u8 = 0;
+                while (self.peek(i) == '#') i += 1;
+                if (i < 1 or i > 6) return null;
+                if (self.peek(i) != ' ') return null;
+                return i;
             },
             else => unreachable,
         }
@@ -100,25 +110,15 @@ pub const Lexer = struct {
         return self.src[self.i] == c;
     }
 
-    fn nextCharIs(self: Self, c: u8) bool {
-        if (self.i + 1 >= self.src.len) return false;
-        return self.src[self.i + 1] == c;
-    }
-
-    /// Determines the degree of the token, if compatible.
-    fn degree(self: Self, t: TokenType) usize {
-        _ = self;
-        switch (t) {
-            .HEADER => {
-                return 1;
-            },
-            else => unreachable,
-        }
+    /// Check ahead without moving the write head.
+    fn peek(self: Self, i: usize) u8 {
+        if (self.i + i >= self.src.len) return 0;
+        return self.src[self.i + i];
     }
 
     /// Moves the read-head forward until 'char' is found.
     fn consumeUntil(self: *Self, char: u8) void {
-        while (self.i < self.src.len and !self.thisCharIs(char)) {
+        while (self.i < self.src.len and self.src[self.i] != char) {
             self.i += 1;
         }
     }
@@ -128,20 +128,78 @@ test "header plain" {
     var arena = std.heap.ArenaAllocator.init(std.testing.allocator);
     defer arena.deinit();
 
-    const input =
-        \\# Header
-        \\plain text.
-    ;
     var l = Lexer.init(arena.allocator());
 
-    const tokens = l.lex(input);
     try expectEqualDeep(&[_]Token{
         .{ .tType = .HEADER, .contents = "# Header\n", .startI = 0, .endI = 9 },
         .{ .tType = .PLAIN, .contents = "plain text.", .startI = 9, .endI = 20 },
-    }, tokens);
+    }, l.lex(
+        \\# Header
+        \\plain text.
+    ));
+    try expectEqualDeep(&[_]Token{
+        .{ .tType = .HEADER, .contents = "# Header with # in the middle", .startI = 0, .endI = 29 },
+    }, l.lex(
+        \\# Header with # in the middle
+    ));
+    try expectEqualDeep(&[_]Token{
+        .{ .tType = .PLAIN, .contents = "Plain with # in the middle", .startI = 0, .endI = 26 },
+    }, l.lex(
+        \\Plain with # in the middle
+    ));
+    try expectEqualDeep(&[_]Token{
+        .{ .tType = .HEADER, .contents = "# A\n", .startI = 0, .endI = 4 },
+        .{ .tType = .PLAIN, .contents = "B\n", .startI = 4, .endI = 6 },
+        .{ .tType = .HEADER, .contents = "# C", .startI = 6, .endI = 9 },
+    }, l.lex(
+        \\# A
+        \\B
+        \\# C
+    ));
+
+    var i: usize = 0;
+    try expectEqualDeep(&[_]Token{
+        .{ .tType = .HEADER, .degree = 1, .contents = "# 1\n", .startI = i, .endI = a: {
+            i += 4;
+            break :a i;
+        } },
+        .{ .tType = .HEADER, .degree = 2, .contents = "## 2\n", .startI = i, .endI = b: {
+            i += 5;
+            break :b i;
+        } },
+        .{ .tType = .HEADER, .degree = 3, .contents = "### 3\n", .startI = i, .endI = c: {
+            i += 6;
+            break :c i;
+        } },
+        .{ .tType = .HEADER, .degree = 4, .contents = "#### 4\n", .startI = i, .endI = d: {
+            i += 7;
+            break :d i;
+        } },
+        .{ .tType = .HEADER, .degree = 5, .contents = "##### 5\n", .startI = i, .endI = e: {
+            i += 8;
+            break :e i;
+        } },
+        .{ .tType = .HEADER, .degree = 6, .contents = "###### 6\n", .startI = i, .endI = f: {
+            i += 9;
+            break :f i;
+        } },
+        .{ .tType = .PLAIN, .degree = 1, .contents = "####### plain", .startI = i, .endI = g: {
+            i += 13;
+            break :g i;
+        } },
+    }, l.lex(
+        \\# 1
+        \\## 2
+        \\### 3
+        \\#### 4
+        \\##### 5
+        \\###### 6
+        \\####### plain
+    ));
 }
 
 const std = @import("std");
+const assert = std.debug.assert;
 const Allocator = std.mem.Allocator;
 const ArrayList = std.ArrayList;
 const expect = std.expect;
