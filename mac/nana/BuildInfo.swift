f69975c5e67db55e273a436759c3798ae3ed64bc
Emmett McDow <emmett.mcdow@gmail.com>
2026-05-30 18:32:13 -0700

    Release 0.2.4
    Notes -> https://emmettmcdow.com/posts/nana-0_2_4

--- mac/nana/BuildInfo.swift
+++ mac/nana/BuildInfo.swift
@@ -3,6 +3,6 @@ import Foundation
 enum BuildInfo {
     static let version: String =
         Bundle.main.object(forInfoDictionaryKey: "CFBundleShortVersionString") as? String ?? "?"
-    static let gitSHA = "1802f29"
+    static let gitSHA = "bf3935a"
     static var label: String { "\(version)+\(gitSHA)" }
 }
